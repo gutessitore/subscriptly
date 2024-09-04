f5a0e46249af55858c8a4a194c99e0267d5d5f10
GuTessitore <gugatessi@gmail.com>
2024-09-04 19:10:40 -0300

    fix: team and vip on circle user list

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -35,13 +35,48 @@ def list_hotmart_users_view(request):
 def extract_hotmart_data(request):
     return extract_subscriptions_view(request)
 
-def compare_users_view(request):
-    hotmart_emails = HotmartSubscription.objects.values_list('subscriber_email', flat=True)
 
-    circle_users_not_in_hotmart = CircleUser.objects.exclude(email__in=hotmart_emails)
+def compare_users_view(request):
+
+    emails_diversity_program = [
+        "kelly.sts17@gmail.com", "alanisesteffanysilvaa@gmail.com",
+        "beatriz.ngonc@gmail.com", "stephmmeireles@gmail.com",
+        "marcellafariasid@gmail.com", "amandabarbozacontato@gmail.com",
+        "heloisa.chs@gmail.com", "dressa.lops@gmail.com",
+        "camylagcelestino@gmail.com", "depaula.stephanny@gmail.com",
+        "juuhcmartins@gmail.com", "alannacarla@outlook.com.br",
+        "oliveiramara0987@gmail.com", "thaisv.franca@gmail.com",
+        "bialaris17@gmail.com", "aanabia033@gmail.com",
+        "angelborges@hotmail.com", "annanaan304@gmail.com",
+        "brunaluisasantos07@gmail.com", "estelamscoelho@gmail.com",
+        "iasmyn.almeida78@gmail.com", "julia.nyte@hotmail.com",
+        "kedmabarroso15@gmail.com", "laurarosa299@gmail.com",
+        "castromayara@outlook.com", "saramirandacontato@hotmail.com",
+        "scoelho684@gmail.com", "valsortiz@yahoo.com.br",
+        "vic.dames@gmail.com"
+    ]
+
+    tags_to_exclude = ['Equipe do Clube', 'Embaixadora']
+
+    hotmart_emails = HotmartSubscription.objects.values_list('subscriber_email', flat=True)
+    circle_users = (
+        CircleUser.objects
+        .exclude(email__in=hotmart_emails)
+        .exclude(email__in=emails_diversity_program)
+    )
+
+    circle_users = [
+        user for user in circle_users if
+        not any(tag in tags_to_exclude for tag in user.tags)
+    ]
+
+    all_tags = set()
+    for user in circle_users:
+        all_tags.update(user.tags)
 
     context = {
-        'circle_users': circle_users_not_in_hotmart
+        'circle_users': circle_users,
+        'unique_tags': sorted(all_tags)
     }
 
     return render(request, 'compare_users.html', context)
